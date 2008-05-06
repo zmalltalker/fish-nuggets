384ca0f5abb9bdbdb387c3be38da8b3af337b742
Marius Mathiesen <marius.mathiesen@gmail.com>
2008-05-06 13:18:15 +0200

    Adding cap completion

---
+++ completions/cap.fish
@@ -0,0 +1,13 @@
+function __cache_or_get_cap_completion -d "Create cap completions"
+    mkdir -p "/tmp/cap_completion_cache"
+	set -l hashed_pwd (md5 -q -s (pwd))
+	set -l cap_cache_file "/tmp/cap_completion_cache/$hashed_pwd"
+	
+	if not test -f "$cap_cache_file"
+    cap -T 2>&1 | grep "^cap" |cut -d " " -f 2 > "$cap_cache_file"
+	end
+	
+	cat "$cap_cache_file"
+end
+
+complete -x -c cap -a "(__cache_or_get_cap_completion)" --description 'Capistrano task'
