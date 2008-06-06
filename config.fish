6b6762a8e96c4292ac2f30873aca223f969c8135
Inge Jørgensen <inge@elektronaut.no>
2008-06-06 04:17:37 +0200

    Added postgres to path

--- config.fish
+++ config.fish
@@ -1,5 +1,5 @@
 if status --is-login
-	for p in /usr/bin /usr/local/bin /opt/local/bin /usr/local/mysql/bin ~/bin ~/.config/fish/bin
+	for p in /usr/bin /usr/local/bin /opt/local/bin /usr/local/mysql/bin /opt/local/lib/postgresql83/bin ~/bin ~/.config/fish/bin 
 		if test -d $p
 			set PATH $p $PATH
 		end
