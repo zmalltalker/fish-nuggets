da48af4a1a5a77bdf449825406fb02318b89db23
August Lilleaas <augustlilleaas@gmail.com>
2008-06-20 14:52:41 +0200

    Rake completion folders are now scoped on user. rake_completion_cache_for_leethal instead of rake_completion_cache. This allows multiple users using the cache at the same time (such as fast user switching in OS X)

--- completions/rake.fish
+++ completions/rake.fish
@@ -1,7 +1,7 @@
 function __cache_or_get_rake_completion -d "Create rake completions"
-    mkdir -p "/tmp/rake_completion_cache"
+    mkdir -p "/tmp/rake_completion_cache_for_$USER"
 	set -l hashed_pwd (md5 -q -s (pwd))
-	set -l rake_cache_file "/tmp/rake_completion_cache/$hashed_pwd"
+	set -l rake_cache_file "/tmp/rake_completion_cache_for_$USER/$hashed_pwd"
 	
 	if not test -f "$rake_cache_file"
 		rake -T 2>&1 | sed -e "/^(/d" -e "s/^rake \([a-z:_0-9!\-]*\).*/\1/" > "$rake_cache_file"
