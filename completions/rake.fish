b00e81b8fd74be74833f6c288b9c6ec64e80ac1b
rflbras <rafael.camargo@gmail.com>
2013-05-08 20:56:23 +0100

    Small fixes on rake.fish
    - Show the correct description of a task.
    - Run the completion only if necessary.

--- completions/rake.fish
+++ completions/rake.fish
@@ -1,13 +1,16 @@
 function __cache_or_get_rake_completion -d "Create rake completions"
-    mkdir -p "/tmp/rake_completion_cache_for_$USER"
-	set -l hashed_pwd (md5pwd)
-	set -l rake_cache_file "/tmp/rake_completion_cache_for_$USER/$hashed_pwd"
-	
-	if not test -f "$rake_cache_file"
-		rake -T 2>&1 | sed -e "/^(/d" -e "s/^rake \([a-z:_0-9!\-]*\).*/\1/" > "$rake_cache_file"
-	end
-	
-	cat "$rake_cache_file"
+  mkdir -p "/tmp/rake_completion_cache_for_$USER"
+  set -l hashed_pwd (md5pwd)
+  set -l rake_cache_file "/tmp/rake_completion_cache_for_$USER/$hashed_pwd"
+
+  if not test -f "$rake_cache_file"
+    rake -T 2>&1 | sed -e "s/^rake \([a-z:_0-9!\-]*\).*#\(.*\)/\1\t\2/" > "$rake_cache_file"
+  end
+  cat "$rake_cache_file"
 end
 
-complete -x -c rake -a "(__cache_or_get_rake_completion)" --description 'Rake Task'
+function __run_rake_completion
+  test -f rakefile; or test -f Rakefile; or test -f rakefile.rb; or test -f Rakefile.rb
+end
+
+complete -x -c rake -a "(__cache_or_get_rake_completion)" -n __run_rake_completion
